dcad8d24f9a50daa4b8be67cf0c07d4ef1d550e9
Farrukh <jfukki@gmail.com>
2016-06-20 21:38:20 +0500

    One Page Website
    One Page Website.HTML CSS JS

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+   
+    
+    $("js--wp-1").waypoint(function(direction) {
+        
+        
+        $("js--wp-1").addClass('animated fadeIn');
+        
+    },{
+        offset:"50%";
+    });
+    
+    
+    
+    
+    
+});
